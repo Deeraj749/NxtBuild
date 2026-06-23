f63261f819b531768da0ec4ce940927405f08887
Deeraj749 <dinakarallu7@gmail.com>
2026-06-23 23:11:40 +0530

    Configure api.js dynamic BASE_URL and fix gitignore

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = import.meta.env.VITE_API_URL 
+  ? `${import.meta.env.VITE_API_URL}/api` 
+  : 'http://localhost:5000/api';
+
 
 const getHeaders = () => {
   const token = Cookies.get('token');

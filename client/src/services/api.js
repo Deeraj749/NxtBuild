75d9633fb086a5a77b4028f00e6b9662ec956566
A.Dinakar <dinakarallu7@gmail.com>
2026-07-06 19:30:23 +0530

    Update BASE_URL to use environment variable

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const BASE_URL = https://nxtbuild-4418.onrender.com/api
+const BASE_URL = import.meta.env.VITE_API_URL 
+  ? `${import.meta.env.VITE_API_URL}/api` 
+  : 'http://localhost:5000/api';
+
 
 
 const getHeaders = () => {
